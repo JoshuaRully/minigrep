bb79a4c294b57ace1fd299a7d93aa090f1d0f7da
Joshua Rully <joshrully@gmail.com>
2023-03-19 12:17:32 -0400

    [main] refactor Config::build to use iterators

--- src/lib.rs
+++ src/lib.rs
@@ -9,28 +9,32 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+    pub fn build(mut args: impl ExactSizeIterator<Item = String>) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("Not enough arguments");
         }
+        // skip first arg as it is binary filepath
+        args.next();
 
-        if args.len() > 3 {
+        let ignore_case: bool;
+        if args.len() > 2 {
             // argument overrides envar
-            let ignore_case = args[1].clone().contains("IGNORE_CASE");
-            let query = args[2].clone();
-            let file_path = args[3].clone();
-
-            return Ok(Config {
-                query,
-                file_path,
-                ignore_case,
-            });
+            ignore_case = match args.next() {
+                Some(arg) => arg.contains("IGNORE_CASE"),
+                None => false,
+            };
+        } else {
+            ignore_case = env::var("IGNORE_CASE").is_ok();
         }
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         Ok(Config {
             query,
